33a514daa2491574dd00c8e53afeee00a9b818cd
Nagisa Ando <amnot.nobig.deal@gmail.com>
2025-03-04 15:43:13 -0600

    test linear search

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,12 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
+// Linear search is O(N), it means that each operation takes the same number of steps as the items in your list
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
 }
