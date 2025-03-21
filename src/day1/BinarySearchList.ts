f9c7f519e15ca8ace9d88105e7a864c67dd462f3
Nagisa Ando <amnot.nobig.deal@gmail.com>
2025-03-21 12:26:33 -0600

    write implementation of binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lowPoint = 0; // lowPoint is inclusive: The element at lowPoint is always included in the search range.
+    let highPoint = haystack.length; // highPoint is exclusive: The element at highPoint is always excluded from the search range.
+    let i = 1;
 
+    do {
+        const midPoint = Math.floor(lowPoint + (highPoint - lowPoint) / 2);
+        const value = haystack[midPoint];
+        i++;
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            highPoint = midPoint;
+        } else {
+            lowPoint = midPoint + 1;
+        }
+    } while (lowPoint < highPoint);
+
+    return false;
 }
